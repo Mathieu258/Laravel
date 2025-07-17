f451d9c5688cc2acf2d696c4cf5c1d593cba1389
Maelle DEGAn <maelledegan06@gmail.com>
2025-07-17 21:44:41 +0200

    Integration des assets pour la mise en forme de la vue

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('pages.dashboad'); });
+Route::get('/login', function () { return view('pages.login'); });
+Route::get('/register', function () { return view('pages.register'); });
